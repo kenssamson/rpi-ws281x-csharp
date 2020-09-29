cc3167715b1c1b6d4255b91bb9fbdeda954cb3e3
d8ahazard <d8ahazard@gmail.com>
2020-09-29 12:06:04 -0500

    Use a more robust clamping method...
    This method (shamelessly stolen from stackoverflow) clamps the colors *much* more accurately, and accounts for pinks and other quite "not-white" light colors.

--- src/rpi_ws281x/ColorUtil.cs
+++ src/rpi_ws281x/ColorUtil.cs
@@ -2,26 +2,48 @@
 using System.Drawing;
 
 namespace rpi_ws281x {
-    public class ColorUtil {
+    public static class ColorUtil {
         /// <summary>
         /// Converts a normal color to one that can be used by RGB+W Strips properly
         /// </summary>
         /// <param name="color">The input color to clamp.</param>
         /// <returns>The adjusted rgb+w color value.</returns>
-        public static Color ClampAlpha(Color color) {
-            // Get the lowest value from our three color values
-            var alpha = Math.Min(color.R, color.G);
-            alpha = Math.Min(alpha, color.B);
-            // If colors are the same, return only the white value
-            if (color.R == color.G && color.R == color.B) {
-                return Color.FromArgb(alpha, 0, 0, 0);
-            }
-            // This is probably not necessary
-            if (color.R + color.G + color.B > 760) {
-                return Color.FromArgb(alpha, 0, 0, 0);
-            }
-            // Return the alpha-modulated version of the color, giving proper white values
-            return Color.FromArgb(alpha, color.R, color.G, color.B);
+        public static Color ClampAlpha(Color tCol) {
+            var rI = tCol.R;
+            var gI = tCol.G;
+            var bI = tCol.B;
+            float tM = Math.Max(rI, Math.Max(gI, bI));
+
+            //If the maximum value is 0, immediately return pure black.
+            if(tM == 0) { return Color.FromArgb(0, 0, 0,0); }
+
+            //This section serves to figure out what the color with 100% hue is
+            var multiplier = 255.0f / tM;
+            var hR = rI * multiplier;
+            var hG = gI * multiplier;
+            var hB = bI * multiplier;  
+
+            //This calculates the Whiteness (not strictly speaking Luminance) of the color
+            var maxWhite = Math.Max(hR, Math.Max(hG, hB));
+            var minWhite = Math.Min(hR, Math.Min(hG, hB));
+            var luminance = ((maxWhite + minWhite) / 2.0f - 127.5f) * (255.0f/127.5f) / multiplier;
+
+            //Calculate the output values
+            var wO = Convert.ToInt32(luminance);
+            var bO = Convert.ToInt32(bI - luminance);
+            var rO = Convert.ToInt32(rI - luminance);
+            var gO = Convert.ToInt32(gI - luminance);
+
+            //Trim them so that they are all between 0 and 255
+            if (wO < 0) wO = 0;
+            if (bO < 0) bO = 0;
+            if (rO < 0) rO = 0;
+            if (gO < 0) gO = 0;
+            if (wO > 255) wO = 255;
+            if (bO > 255) bO = 255;
+            if (rO > 255) rO = 255;
+            if (gO > 255) gO = 255;
+            return Color.FromArgb(wO, rO, gO, bO);
         }
     }
 }
